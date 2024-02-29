003a9bc68f9b55b94a80934290b598ade60e3b53
Joeljac0 <jacobsson.joel5@gmail.com>
2024-02-29 14:56:19 +0100

    yuh
    yuh

--- views/scr.js
+++ views/scr.js
@@ -2,13 +2,26 @@ let day = 0
 
 const weather_container = document.getElementById("weather_container")
 
+weather_container.children[day].classList.remove("hidden")
 weather_container.children[day].classList.add("shown")
 
 
-function change_day(amount) {
+function next_day() {
+    if (day != 5) {
+        weather_container.children[day].classList.remove("shown")
+        weather_container.children[day].classList.add("hidden")
+        day++
+        weather_container.children[day].classList.remove("hidden")
+        weather_container.children[day].classList.add("shown")
+    }
+}
 
-    weather_container.children[day].classList.remove("shown")
-    day += amount
-    weather_container.children[day].classList.add("shown")
-
+function prev_day() {
+    if (day != 0) {
+        weather_container.children[day].classList.remove("shown")
+        weather_container.children[day].classList.add("hidden")
+        day--
+        weather_container.children[day].classList.remove("hidden")
+        weather_container.children[day].classList.add("shown")
+    }
 }
